3ea2f39170765fa7def05e478a0c559545ccea6a
gregory.durand <gregory.durand@he-arc.ch>
2018-10-14 19:07:38 +0200

    update methode greet(String name)

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,7 +1,13 @@
 public class Greeting {
     public String greet(String name) {
+        String greeting;
 
+        if (name == null) {
+            greeting = "Hello, my friend";
+        } else {
+            greeting = "Hello, " + name;
+        }
 
-        return "Hello, " + name;
+        return greeting;
     }
 }
